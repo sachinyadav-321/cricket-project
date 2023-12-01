e46d3d454320f06d927ce20e17c0ecf1e7e7ed40
sachinyadav-321 <sachinyad9958@gmail.com>
2023-12-01 13:40:40 +0530

    Add New Changes

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-let score  = {
-    win:0,
-    lost:0,
-    tie:0,
-};
+let scoreStr = localStorage.getItem('Score');
+let score;
+resetScore(scoreStr);
+
+function resetScore(scoreStr) {
+    score = scoreStr ? JSON.parse(scoreStr) : {
+        win: 0,
+        lost: 0,
+        tie: 0,
+    };
+    score.displayScore = function () {
+        return `Game-Score :  Won : ${score.win} | Lost : ${score.lost} | Tie : ${score.tie}`;
+    };
+
+    showResult();
+}
+
+
 
 function generateComputerChoice() {
     let randomNum = Math.random() * 3;
@@ -17,42 +30,43 @@ function generateComputerChoice() {
         return 'Stump';
     }
 }
-function getResult(userMove,computerMove) {
-    if (userMove=== 'Bat') {
-        if (computerMove=== 'Ball') {
+
+function getResult(userMove, computerMove) {
+    if (userMove === 'Bat') {
+        if (computerMove === 'Ball') {
             score.win++;
             return 'User Won';
         }
-        else if (computerMove=== 'Bat') {
+        else if (computerMove === 'Bat') {
             score.tie++;
             return `It's a Tie`;
-        }else if (computerMove=== 'Stump') {
+        } else if (computerMove === 'Stump') {
             score.lost++;
             return 'Computer Won';
         }
     }
-    else if (userMove=== 'Ball') {
-        if (computerMove=== 'Ball') {
+    else if (userMove === 'Ball') {
+        if (computerMove === 'Ball') {
             score.tie++;
             return `It's a Tie`;
         }
-        else if (computerMove=== 'Bat') {
+        else if (computerMove === 'Bat') {
             score.lost++;
             return 'Computer Won';
-        }else if (computerMove=== 'Stump') {
+        } else if (computerMove === 'Stump') {
             score.win++;
             return 'User Won';
         }
     }
-    if (userMove=== 'Stump') {
-        if (computerMove=== 'Ball') {
+    if (userMove === 'Stump') {
+        if (computerMove === 'Ball') {
             score.lost++;
             return 'Computer Won';
         }
-        else if (computerMove=== 'Bat') {
+        else if (computerMove === 'Bat') {
             score.win++;
             return 'User Won';
-        }else if (computerMove=== 'Stump') {
+        } else if (computerMove === 'Stump') {
             score.tie++;
             return `It's a Tie`;
         }
@@ -60,7 +74,18 @@ function getResult(userMove,computerMove) {
 
 }
 
-function showResult(userMove,computerMove,result) {
-    alert(`User Choose ${userMove}.   |  Computer Choose ${computerMove}.   |  ${result}.    
-            Won : ${score.win}            Lost : ${score.lost}                Tie : ${score.tie}`);
+function showResult(userMove, computerMove, result) {
+
+    localStorage.setItem('Score', JSON.stringify(score));
+    alert(`You have chosen ${userMove}  | Computer Chosen ${computerMove} | ${result}
+        
+        ${score.displayScore()}`)
+
+    // document.querySelector('#user-move').innerText = userMove ? `You have chosen ${userMove}` : '';
+
+    // document.querySelector('#computer-move').innerText = computerMove ? `You have chosen ${computerMove}` : '';
+
+    // document.querySelector('#result').innerText = result || '';
+
+    // document.querySelector('#score').innerText = score.displayScore();
 }
